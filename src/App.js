eff677d9646c268b47849d86ac81076b3c381cc0
faog <fabiola.orellana.g@gmail.com>
2019-06-20 14:29:25 -0400

    Estilos iniciales de la barra nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import ComponentVisualMap from './components/componentVisualMap';
+import logo from './img/logo-digital-generation.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <ComponentVisualMap />
+      <React.Fragment>
+        <header>
+          <nav className="navbar navbar-expand-lg navbar-light">
+            <img src={logo} alt="Logo" />
+          </nav>
+        </header>
+        <ComponentVisualMap />
+      </React.Fragment>
     );
   }
 }
