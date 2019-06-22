8db8f095529dc5bf9e8d2738e7b05ef63fc52f18
faog <fabiola.orellana.g@gmail.com>
2019-06-22 18:19:58 -0400

    Corrección para testear el componente <App> a partir de la librería de google maps para test

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,17 @@
 /* eslint-disable no-undef */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import createGoogleMapsMock from 'jest-google-maps-mock';
 import App from './App';
 
+configure({ adapter: new Adapter() });
+window.google = jest.fn();
+window.google.maps = createGoogleMapsMock();
+
 describe('App', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  it('Debería existir el componente <App />', () => {
+    const component = mount(<App />);
+    expect(component.exists()).toBe(true);
   });
 });
