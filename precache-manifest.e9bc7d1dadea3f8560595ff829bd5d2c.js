3141a850d80c30d26c52f8e7a1195e4ec5393460
faog <fabiola.orellana.g@gmail.com>
2019-06-22 19:22:01 -0400

    Updates

---
+++ precache-manifest.e9bc7d1dadea3f8560595ff829bd5d2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e96c0d31a83c9fe9c3b88955699e513",
+    "url": "/SCL008_DigitalGeneration/index.html"
+  },
+  {
+    "revision": "581a2d85eae92a6b7c0e",
+    "url": "/SCL008_DigitalGeneration/static/css/main.1ea979b9.chunk.css"
+  },
+  {
+    "revision": "e1df9e5dd97f9dc5b41a",
+    "url": "/SCL008_DigitalGeneration/static/js/2.373ccd69.chunk.js"
+  },
+  {
+    "revision": "581a2d85eae92a6b7c0e",
+    "url": "/SCL008_DigitalGeneration/static/js/main.a7f12110.chunk.js"
+  },
+  {
+    "revision": "fb8fa25cd25759e88b8f",
+    "url": "/SCL008_DigitalGeneration/static/js/runtime~main.223afaef.js"
+  },
+  {
+    "revision": "8e0c234ad8b7b3c4b90914419cd9e191",
+    "url": "/SCL008_DigitalGeneration/static/media/logo-digital-generation.8e0c234a.png"
+  }
+]);
