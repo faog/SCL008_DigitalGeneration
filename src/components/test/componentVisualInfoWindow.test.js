31d9c83abd110fb0f354e4ad04fa4cd72be398d4
faog <fabiola.orellana.g@gmail.com>
2019-06-23 16:13:21 -0400

    Corrección test para componentVisualInfoWindow

--- src/components/test/componentVisualInfoWindow.test.js
+++ src/components/test/componentVisualInfoWindow.test.js
@@ -5,15 +5,25 @@ import Adapter from 'enzyme-adapter-react-16';
 import createGoogleMapsMock from 'jest-google-maps-mock';
 import ComponentVisualInfoWindow from '../componentVisualInfoWindow';
 
-const storeDirectory = require('../../serviceWorker');
+const storeDirectory = require('../../store_directory');
+
+const marker = {
+  latLng: {
+    lat: () => (19.50548),
+    lng: () => (-99.15066),
+  },
+};
 
 configure({ adapter: new Adapter() });
 window.google = jest.fn();
 window.google.maps = createGoogleMapsMock();
 
-describe('App', () => {
+describe('ComponentVisualInfoWindow', () => {
   it('Debería existir el componente <ComponentVisualInfoWindow />', () => {
-    const component = mount(<ComponentVisualInfoWindow storeDirectory={storeDirectory} />);
+    const component = mount(<ComponentVisualInfoWindow
+      storeDirectory={storeDirectory}
+      marker={marker}
+    />);
     expect(component.exists()).toBe(true);
   });
 });
