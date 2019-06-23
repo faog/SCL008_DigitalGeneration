d0b42a2d797afc8cfc8fcea9db259981ffe5d0e5
faog <fabiola.orellana.g@gmail.com>
2019-06-22 22:49:22 -0400

    Estilos para el componentVisualInfoWindow

--- src/components/componentVisualInfoWindow.js
+++ src/components/componentVisualInfoWindow.js
@@ -3,6 +3,7 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
 import React, { Component } from 'react';
+import './css/componentVisualInfoWindow.css';
 
 class ComponentVisualInfoWindow extends Component {
   constructor(props) {
@@ -20,19 +21,25 @@ class ComponentVisualInfoWindow extends Component {
     const markerData = this.getMarkerData(this.props.marker);
     return (
       <div id="insideinfowindow">
-        información:
-        Nombre:
-        {' '}
-        {markerData.Name}
-        Dirección:
-        {' '}
-        {markerData.Address}
-        Coordenadas:
-        {' '}
-        {markerData.Coordinates.lat}
-,
-        {' '}
-        {markerData.Coordinates.lng}
+        <h4>INFORMACIÓN TIENDA</h4>
+        <h5>
+          Nombre:
+          {' '}
+          {markerData.Name}
+        </h5>
+        <h5>
+          Dirección:
+          {' '}
+          {markerData.Address}
+        </h5>
+        <h5>
+          Coordenadas:
+          {' '}
+          {markerData.Coordinates.lat}
+          ,
+          {' '}
+          {markerData.Coordinates.lng}
+        </h5>
       </div>
     );
   }
