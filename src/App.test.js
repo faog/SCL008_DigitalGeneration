37237c6ac08eeea41505867de94dadee0a5550da
faog <fabiola.orellana.g@gmail.com>
2019-06-23 17:42:45 -0400

    Corrección test para este componente, debido a la implementación de redux

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,9 @@ import React from 'react';
 import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import createGoogleMapsMock from 'jest-google-maps-mock';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/store';
 
 configure({ adapter: new Adapter() });
 window.google = jest.fn();
@@ -11,7 +13,7 @@ window.google.maps = createGoogleMapsMock();
 
 describe('App', () => {
   it('Debería existir el componente <App />', () => {
-    const component = mount(<App />);
+    const component = mount(<Provider store={store}><App /></Provider>);
     expect(component.exists()).toBe(true);
   });
 });
