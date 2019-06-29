bf113246b460450b2ffae3c1dd3ea60a7387c501
faog <fabiola.orellana.g@gmail.com>
2019-06-29 18:21:06 -0400

    lectura del estado inicial y guardado de marcadores, usando localStorage

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,10 +2,22 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 import reducer from '../reducers';
-import state from './initialState';
+import { loadState, saveState } from './initialState';
 
-export default createStore(
+const initialData = loadState();
+const store = createStore(
   reducer,
-  state,
-  composeWithDevTools(applyMiddleware(thunkMiddleware)),
+  initialData,
+  composeWithDevTools(
+    applyMiddleware(thunkMiddleware),
+  ),
 );
+/*
+  subscribe(): es una función del store de redus, que permite ejecutar la función
+que recibe como parámetro cada vez que ocurre un cambio de estado
+*/
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
+export default store;
--- src/redux/store/initialState.js
+++ src/redux/store/initialState.js
@@ -1,3 +1,31 @@
-export default {
-
+/* eslint-disable no-console */
+/**
+  loadState: carga el estado desde localStorage (sólo permite string)
+  saveState: guardo el estado inicial que esta en localStorage
+  Si encuentra un state, este debe ser convertido de string a un objeto de estado redux,
+  ya que localStorage solo almacena string; usando JSON.parse();
+ */
+export const loadState = () => {
+  try {
+    const savedState = localStorage.getItem('state');
+    if (savedState === null) {
+      return undefined;
+    }
+    return JSON.parse(savedState);
+  } catch (error) {
+    return undefined;
+  }
+};
+/**
+  saveState: guarda el estado en localStorage
+  stateToSave: almacena el state de redux como string JSON, indicando que
+  será guardado en estas condiciones
+  */
+export const saveState = (state) => {
+  try {
+    const stateToSave = JSON.stringify(state);
+    localStorage.setItem('state', stateToSave);
+  } catch (error) {
+    console.log(`Error para guardar el estado: ${error}`);
+  }
 };
